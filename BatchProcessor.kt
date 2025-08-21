6afa450e16175bcfa7cb1eab2c63fb62fa94e22b
milianlo <maxigood1000@yahoo.com>
2025-08-21 20:19:54 +0100

    Create BatchProcessor.kt

---
+++ BatchProcessor.kt
@@ -0,0 +1,9 @@
+package Lenstules
+
+// Batch processor for applying lens styles to multiple images
+class BatchProcessor {
+    fun processBatch(profiles: List<LensProfile>, images: List<Any>): List<Any> {
+        // TODO: Apply each profile to each image and return result
+        return images
+    }
+}
