e408f105756614b821f35c6334b414ace07fba27
milianlo <maxigood1000@yahoo.com>
2025-08-21 20:18:53 +0100

    Create AnimationStub.kt

---
+++ AnimationStub.kt
@@ -0,0 +1,9 @@
+package Lenstules
+
+// Animation stub for future animated lens effects
+class AnimationStub {
+    fun animateLens(profile: LensProfile, image: Any): Any {
+        // TODO: Implement animation logic
+        return image
+    }
+}
